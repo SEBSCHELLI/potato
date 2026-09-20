53facfa29c56779c2cd74de40af134d3fd21517b
Sebastian Schellhammer <sebastian.schellhammer@gesis.org>
2026-09-20 15:56:08 +0200

    add 2vs1 adjudication mode

--- potato/routing/annotation.py
+++ potato/routing/annotation.py
@@ -277,7 +277,7 @@ def _2vs1_adjudicate(username, session_id, user_state, all_user_states_for_cur_u
     instance_labels = []
     for annotator_id in instance_annotators:
         annotator_us = get_user_state_manager().get_all_user_states(annotator_id)
-        for us in annotator_us:
+        for _, us in annotator_us.items():
             annotations = us.instance_id_to_label_to_value.get(instance_id, None)
             if annotations is not None:
                 annotator_instance_label = []
@@ -293,6 +293,8 @@ def _2vs1_adjudicate(username, session_id, user_state, all_user_states_for_cur_u
                 logger.debug(f"Found annotation {annotator_instance_label} from User {us.user_id}")
                 continue
 
+    instance_labels = "; ".join(instance_labels)
+    instance_text = instance_text + "\n\n" + instance_labels
     # Calculate progress counter values
     # Get the number of completed annotations and remaining assignable items
     finished_count = user_state.get_annotation_count()
